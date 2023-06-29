22d3abc77f4f17f6f4d7e91f214a56395c4370e2
KIRAN FAKIRA INGALE <75682892+kiraningale2001@users.noreply.github.com>
2023-06-29 17:54:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='wkcounter',
+    name='Word_Frequency_Counter',
     version='1.0',
     packages=find_packages(),
+    packages=['Word_Frequency_Counter'],
     install_requires=[
     ],
     # Add additional configuration as needed
