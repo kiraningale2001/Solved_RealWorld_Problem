466d9faa73270c33b9b7f7da607509e762d818c7
KIRAN FAKIRA INGALE <75682892+kiraningale2001@users.noreply.github.com>
2023-06-29 17:28:27 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='word_frequency',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'os'
+    ],
+    # Add additional configuration as needed
+)
